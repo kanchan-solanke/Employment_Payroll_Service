8a39f61fc808fef796ed50ffee4a805bb439877f
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-23 22:34:10 +0530

    create a table employee payroll

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,3 +1,8 @@
 >  create database payroll_Service;
    show databases;
    use payroll_Service;
+
+>
+use payroll_Service;
+create Table employee_payroll(id int auto_increment, name varchar(50), salary varchar(50),
+ start_date varchar(30),primary key (id));
