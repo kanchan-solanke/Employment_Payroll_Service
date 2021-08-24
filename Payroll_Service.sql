0e37da0cfab50f50e9f1fc82cc33949186b22bc1
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-24 07:51:02 +0530

    insert into employee_payroll

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -6,3 +6,9 @@
 use payroll_Service;
 create Table employee_payroll(id int auto_increment, name varchar(50), salary varchar(50),
  start_date varchar(30),primary key (id));
+
+>
+insert into employee_payroll
+values(1,'Raj', '10000','21-03-2020'),
+(2,'Meher','20000','2-05-2020'),
+(3,'Rahul','30000','6-07-2021');
